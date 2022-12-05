429ac7211778895567a34c065308e3bffaaa3065
Robert Jugaru <robertgeorgejugaru@gmail.com>
2022-12-05 11:11:58 +0200

    Day 2

--- AdventOfCode.Solutions/Year2022/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day02/Solution.cs
@@ -1,16 +1,69 @@
+using AdventOfCode.Solutions.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2022.Day02;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(02, 2022, "") { }
+    private readonly Dictionary<string, int> combinations = new()
+    {
+        { "A X", 4 },
+        { "A Y", 8 },
+        { "A Z", 3 },
+
+        { "B X", 1 },
+        { "B Y", 5 },
+        { "B Z", 9 },
+
+        { "C X", 7 },
+        { "C Y", 2 },
+        { "C Z", 6 }
+    };
+
+    private readonly Dictionary<string, string> replacements = new()
+    {
+        { "A X", "A Z" },
+        { "A Y", "A X" },
+        { "A Z", "A Y" },
+
+        { "B X", "B X" },
+        { "B Y", "B Y" },
+        { "B Z", "B Z" },
+
+        { "C X", "C Y" },
+        { "C Y", "C Z" },
+        { "C Z", "C X" }
+    };
+
+    public Solution() : base(02, 2022, "Rock Paper Scissors") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        List<string> lines = Input.SplitByNewline(true, true).ToList();
+        long sum = 0;
+        foreach(string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                sum += combinations[line];
+            }
+        }
+        return sum.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        List<string> lines = Input.SplitByNewline(true, true).ToList();
+        long sum = 0;
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                sum += combinations[replacements[line]];
+            }
+        }
+        return sum.ToString();
     }
 }
